ad732553b1c6a38fc1f9dc0f45b31437f18984c2
saquib156 <saquibjamal.jamal156@gmail.com>
2021-08-29 09:35:53 +0530

    file up

--- script.js
+++ script.js
@@ -5,10 +5,6 @@ let count = 0;
 const count_val = document.querySelector("#counter");
 const btns  = document.querySelectorAll(".btn");
 
-/*var reset_btn = document.querySelector("#reset");
-
-var inc_btn = document.querySelector("#increase");*/
-
 btns.forEach(function(btn){
     btn.addEventListener("click", function(e){
         const field_id = e.currentTarget.classList;
@@ -33,28 +29,3 @@ btns.forEach(function(btn){
         count_val.textContent = count;
     });
 });
-
-// Function for Increase/Decrease/Reset
-/*function decrease(){
-    var count = document.getElementById("counter").textContent;
-    count -= 1;
-    document.getElementById("counter").innerHTML = count;
-}
-
-function increase(){
-    var count = document.getElementById("counter").textContent;
-    count = Number(count);
-    count += 1;
-    document.getElementById("counter").innerHTML = count;
-}
-
-function reset(){
-    document.getElementById("counter").innerHTML = 0;
-}*/
-
-//Event for Increase/Decrease/Reset
-/*dec_btn.addEventListener("click", decrease);
-
-inc_btn.addEventListener("click", increase);
-
-reset_btn.addEventListener("click", reset);*/
